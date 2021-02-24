da5d5785af2f0fa589f34a9f8f61ab38e5b2217e
EricNshimiyimana <75353826+EricNshimiyimana@users.noreply.github.com>
2021-02-24 21:30:02 +0200

    Update test_news.py
    Update file

--- tests/test_news.py
+++ tests/test_news.py
@@ -27,6 +27,4 @@ class ArticlesTest(unittest.TestCase):
         self.new_article = Articles('Gizmode.com','Jennings Brown','Man Claims He Invented Bitcoin, Is Ordered to Pay Billions in Bitcoin','A man who has insisted he is the man behind the pseudonymous identity of Satoshi Nakamoto, inventor of bitcoin, has been ordered to pay half of his cryptocurrency bounty to a man believed to be his former colleague','https://gizmodo.com/man-claims-he-invented-bitcoin-is-ordered-to-pay-billi-1837659816','https://i.kinja-img.com/gawker-media/image/upload/s--H8pqYMUW--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/ug34lxszlekl8efydtj3.png','2019-08-28T16:50:00Z')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_article,Articles))
-        
-      
+        self.assertTrue(isinstance(self.new_article,Articles)
